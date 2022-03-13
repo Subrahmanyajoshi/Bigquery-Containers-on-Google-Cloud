226261892e8fa750d2af69dfd8cfdf810953c7e4
Subrahmanya Joshi <subrahmanyajoshi123@gmail.com>
2022-03-13 09:41:24 +0530

    Add cloud logging and Fix bugs
    1. Cloud logging works now
    2. Setting environment variable inside the dockerfile doesn't work. It needs to be set inside the python script programmatically.

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,5 @@ COPY dockerize /opt/
 WORKDIR /opt/
 
 RUN pip install -r requirements.txt
-RUN export GOOGLE_APPLICATION_CREDENTIALS='/opt/key.json'
-
 ENTRYPOINT ["python3", "bigquery_pusher.py"]
 CMD []
--- dockerize/bigquery_pusher.py
+++ dockerize/bigquery_pusher.py
@@ -1,11 +1,18 @@
 import os
 import argparse
 
-from google.cloud import bigquery, logging
+from google.cloud import bigquery
 from google.cloud.exceptions import NotFound
 
-logging_client = logging.Client()
-logger = logging_client.logger("projects/text-analysis-323506/logs/containers-log")
+import logging
+import google.cloud.logging
+from google.cloud.logging.handlers import CloudLoggingHandler
+
+client = google.cloud.logging.Client(project='text-analysis-323506')
+handler = CloudLoggingHandler(client)
+cloud_logger = logging.getLogger('cloudLogger')
+cloud_logger.setLevel(logging.INFO)
+cloud_logger.addHandler(handler)
 
 class BigqueryPusher(object):
 
@@ -27,7 +34,7 @@ class BigqueryPusher(object):
             self.bq_client.get_dataset(self.database_id)
         except NotFound:
             self.create_database()
-            
+
         try:
             self.dest_table = self.bq_client.get_table(self.table_id)
         except NotFound:
@@ -39,16 +46,18 @@ class BigqueryPusher(object):
         dataset = bigquery.Dataset(self.database_id)
         dataset.location = "us-east1"
         dataset = self.bq_client.create_dataset(dataset, timeout=30)
-        # logger.log_text(f"Created dataset {self.bq_client.project}.{dataset.dataset_id}")
+        cloud_logger.info(f"Created dataset {self.bq_client.project}.{dataset.dataset_id}")
 
     def create_table(self):
         """ Creates bigquery table """
         table = bigquery.Table(self.table_id, schema=self.table_schema)
         table = self.bq_client.create_table(table)
-        # logger.log_text(f"Created table {table.project}.{table.dataset_id}.{table.table_id}")
+        cloud_logger.info(f"Created table {table.project}.{table.dataset_id}.{table.table_id}")
 
     def insert(self):
         """ Insert values into the specified table """
+
+        cloud_logger.info("Total number of rows in the table before inserting {} ".format(self.dest_table.num_rows))
 
         # Bigquery load job configuration
         job_config = bigquery.LoadJobConfig(
@@ -67,7 +76,8 @@ class BigqueryPusher(object):
 
         # Waiting for inserting to complete
         load_job.result()
-        # logger.log_text("Inserted {} rows".format(self.dest_table.num_rows))
+        self.dest_table = self.bq_client.get_table(self.table_id)
+        cloud_logger.info("Total number of rows in the table after inserting {} ".format(self.dest_table.num_rows))
 
 
 if __name__ == '__main__':
@@ -76,6 +86,10 @@ if __name__ == '__main__':
                         help="Gcs path of the csv file beginning with 'gs://'")
     args = parser.parse_args()
 
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/opt/key.json'
+    key_path = '/opt/key.json'
+    if not os.path.exists(key_path):
+        raise ValueError(f"Google Cloud service account key path {key_path} doesn't exist")
+
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = key_path
     bq_pusher = BigqueryPusher(args.gcs_path)
     bq_pusher.insert()
